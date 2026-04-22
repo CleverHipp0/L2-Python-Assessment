ce68ed6cf0a1737c6a5077e0a2bff122da67a884
CleverHipp0 <lilec3056@masseyhigh.school.nz>
2026-04-23 10:18:02 +1200

    Recycled from Project 0

---
+++ R_03_Yes_no_checker_V2.py
@@ -0,0 +1,29 @@
+def yes_no(inquiry):
+    """An even simpler version of my original yes no checker. Asks a question and
+    checks if the answer is yes or no."""
+
+    # Error message
+    error = "🚨 ERROR: This Field is required. Please enter a 'yes' or 'no' response. 🚨"
+
+    # Repats the question like a pesky child until it is correctly answered.
+    while True:
+        response = input(inquiry).lower().strip()
+
+        # Compares the answer to see whether it is a yes or no.
+        if response == "yes" or response == "y":
+            return "yes"
+
+        elif response == "no" or response == "n":
+            return "no"
+
+        # If there is no match print an error.
+        else:
+            print(error)
+
+
+# Main routine goes here.
+
+if yes_no("Do you like com sci? ") == "no":
+    print("Wrong answer")
+else:
+    print("Correct")
